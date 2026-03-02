e7275778c4a16caaaa87d8580ae63f16e209bfd2
JoseGB3 <jose.gb3@puccampinas.edu.br>
2026-03-02 18:10:12 -0300

    Add files via upload

---
+++ ex3l2.py
@@ -0,0 +1,5 @@
+cotacao=float(input("Digite o valor da atual cotação do dolar: "))
+valor_dolar=float(input("Digite um valor em dolar: "))
+valor_reais=cotacao*valor_dolar
+print(f"O valor convertido de dolar para real é: {valor_reais:.2f} R$")
+
